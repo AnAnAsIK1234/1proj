e9ae0395dff1f31e71b09332b94635570abf6e91
AnAnAsIK1234 <stas.guschin471@gmail.com>
2024-07-13 15:43:43 +0300

    changes

--- leet/leet.py
+++ leet/leet.py
@@ -26,3 +26,17 @@ def BinarySearch(mass, key):
             lo = mi + 1
     return -1
 
+
+#UpperBound
+def BinarySearch(mass, key):
+    lo = 0
+    hi = len(mass) - 1
+    while (lo <= hi):
+        mi = lo + (hi - lo) // 2
+        if mass[mi] == key:
+            return mi
+        elif key < mass[mi]:
+            hi = mi - 1
+        else:
+            lo = mi + 1
+    return -1
---
+++ readme.txt
@@ -0,0 +1,15 @@
+#Previoulsy you must cd 1proj
+#git add namefile
+#git commit -m "описание изменений"
+#git push
+#12345
+
+#get
+#git pull
+
+#changes
+#git add -A
+#git commit -m "changes"
+#git push
+
+#если попалось какое-то окошко, то esp + :wq
